b64625fdd064436548191b7b5100fff079b266fa
XanOnlin3 <xanonlin3@gmail.com>
2014-03-25 12:54:31 -0400

    game project added

---
+++ RunningfromCertainDeath/RunningfromCertainDeath/RunningfromCertainDeath/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace RunningfromCertainDeath
+{
+    class MenuItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Action<Buttons> Action { get; set; }
+    }
+}
